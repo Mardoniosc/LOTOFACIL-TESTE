21c729ef8ba7004f579f7210a4bc6169789fc3d4
Mardonio S Costa <35425708+Mardoniosc@users.noreply.github.com>
2025-04-04 17:42:34 -0300

    ATUALIZADO EXCEL

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import random
+import requests
+
+def download_excel_file(url, output_path):
+    # Baixar o arquivo Excel
+    response = requests.get(url)
+    with open(output_path, 'wb') as file:
+        file.write(response.content)
 
 def read_excel_file(file_path):
     # Lê o arquivo Excel
@@ -8,12 +15,10 @@ def read_excel_file(file_path):
 
 def analyze_results(df):
     # Analisar os resultados anteriores para identificar padrões
-    # Aqui você pode implementar a lógica de análise que preferir
-    # Por exemplo, calcular a frequência de cada número sorteado
     frequency = df.stack().value_counts()
     return frequency
 
-def generate_possible_games(frequency, num_games=10):
+def generate_possible_games(frequency, previous_results, num_games=10):
     # Garantir que todos os números sejam inteiros e válidos
     all_numbers = []
     for item in frequency.index:
@@ -25,23 +30,35 @@ def generate_possible_games(frequency, num_games=10):
             continue
     
     possible_games = []
-    for _ in range(num_games):
-        game = random.sample(all_numbers[:25], 15)  # Seleciona 15 números aleatórios dos 25 mais frequentes
-        possible_games.append(sorted(game))
+    while len(possible_games) < num_games:
+        game = sorted(random.sample(all_numbers[:25], 15))  # Seleciona 15 números aleatórios dos 25 mais frequentes
+        if game not in previous_results:
+            possible_games.append(game)
     return possible_games
 
 def main():
-    # Caminho para o arquivo Excel
-    file_path = 'Lotofácil.xlsx'
+    # URL do arquivo Excel no GitHub
+    url = 'https://github.com/Mardoniosc/LOTOFACIL-TESTE/blob/master/Lotof%C3%A1cil.xlsx?raw=true'
+    output_path = 'Lotofácil.xlsx'
+    
+    # Baixar o arquivo Excel
+    download_excel_file(url, output_path)
     
     # Ler o arquivo Excel
-    df = read_excel_file(file_path)
+    df = read_excel_file(output_path)
+    
+    # Filtrar apenas as colunas que contêm os números sorteados
+    number_columns = [col for col in df.columns if df[col].dtype == 'int64' or df[col].dtype == 'float64']
+    df_numbers = df[number_columns]
     
     # Analisar os resultados
-    frequency = analyze_results(df)
+    frequency = analyze_results(df_numbers)
+    
+    # Extrair os resultados anteriores
+    previous_results = [sorted(list(map(int, row.dropna().tolist()))) for _, row in df_numbers.iterrows()]
     
     # Gerar jogos possíveis
-    possible_games = generate_possible_games(frequency)
+    possible_games = generate_possible_games(frequency, previous_results)
     
     for game in possible_games:
         print(game)
